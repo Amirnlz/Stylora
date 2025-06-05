98b45922be5d6aa76fabf5e5e68bdbd1e9693b07
Amir <amiirnlz79@gmail.com>
2025-06-05 22:33:51 +0330

    Fix: fix the image loading issues

--- app/src/main/java/com/amirnlz/stylora/pages/feedback/ui/FeedbackNetworkImage.kt
+++ app/src/main/java/com/amirnlz/stylora/pages/feedback/ui/FeedbackNetworkImage.kt
@@ -1,11 +1,14 @@
 package com.amirnlz.stylora.pages.feedback.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -21,24 +24,33 @@ fun FeedbackNetworkImage(
 ) {
     val context = LocalContext.current
     val imageUrl = remember { imageUrl }
+    Log.d("FeedbackNetworkImage", "Loading image from URL: $imageUrl")
 
     val requestBuilder = remember(context, imageUrl) {
         ImageRequest.Builder(context)
-            .data(imageUrl)
+            .data(imageUrl.replace("http://", "https://"))
             .crossfade(300)
             .diskCachePolicy(CachePolicy.ENABLED)
             .memoryCacheKey(imageUrl)
             .diskCacheKey(imageUrl)
             .crossfade(300)
+            .listener(
+                onStart = { Log.d("Coil", "Loading started") },
+                onError = { _, throwable ->
+                    Log.e("Coil", "Error loading image: $throwable")
+                },
+                onSuccess = { _, _ -> Log.d("Coil", "Loaded successfully") }
+            )
     }
 
     AsyncImage(
         model = requestBuilder.build(),
         contentDescription = "FeedbackResponse image",
         contentScale = ContentScale.Fit,
+        placeholder = ColorPainter(Color.LightGray),
+        error = ColorPainter(Color.Red),
         modifier = Modifier
             .size(size.dp)
             .clip(RoundedCornerShape(8.dp))
     )
-
 }
